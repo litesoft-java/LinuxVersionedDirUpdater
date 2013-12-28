138f586d1b95355b9da063b0d30d3ec1f57f548d
George A Smith <litesoft@gmail.com>
2013-12-28 14:23:26 -0800

    Dir Purge

--- src/org/litesoft/linuxversioneddirupdater/utils/DirectoryUtils.java
+++ src/org/litesoft/linuxversioneddirupdater/utils/DirectoryUtils.java
@@ -7,10 +7,12 @@ public class DirectoryUtils
 {
     public static boolean existsThenAssertMutable( File pTargetPath, String pWhy )
     {
-        if (!pTargetPath.exists()) {
+        if ( !pTargetPath.exists() )
+        {
             return false;
         }
-        if (!acceptableMutable( pTargetPath )) {
+        if ( !acceptableMutable( pTargetPath ) )
+        {
             throw new IllegalArgumentException( pWhy + pTargetPath.getAbsolutePath() );
         }
         return true;
@@ -22,7 +24,45 @@ public class DirectoryUtils
     }
 
     public static void purge( File pDirectory )
+            throws FileSystemException
     {
+        Objects.assertNotNull( "Directory", pDirectory );
+        if ( pDirectory.isDirectory() )
+        {
+            LowLevelDeleteAllEntries( pDirectory );
+            if ( !pDirectory.delete() || pDirectory.exists() )
+            {
+                throw new FileSystemException( "Unable to delete: " + pDirectory.getAbsolutePath() );
+            }
+        }
+    }
 
+    @SuppressWarnings({"UnusedDeclaration"})
+    public static void deleteAllEntries( File pDirectory )
+            throws FileSystemException
+    {
+        Objects.assertNotNull( "Directory", pDirectory );
+        if ( pDirectory.isDirectory() )
+        {
+            LowLevelDeleteAllEntries( pDirectory );
+        }
+    }
+
+    private static void LowLevelDeleteAllEntries( File pDirectory )
+            throws FileSystemException
+    {
+        String[] files = pDirectory.list();
+        for ( String file : files )
+        {
+            File entry = new File( pDirectory, file );
+            if ( entry.isDirectory() )
+            {
+                deleteIfExists( entry );
+            }
+            else
+            {
+                FileUtils.deleteIfExists( entry );
+            }
+        }
     }
 }
